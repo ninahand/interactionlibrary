cd79a654a3b6423e8f170096112f034dc362dcf2
ninahand <ninahand@newschool.edu>
2024-02-19 12:02:48 -0500

    updates (buttons/testing fonts)

---
+++ page-two/script.js
@@ -0,0 +1,10 @@
+var svgButton = document.getElementById('svgButton');
+var timeleft = 3;
+var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("timer").textContent = timeleft;
+    if(timeleft <= 0) {
+        clearInterval(downloadTimer);
+        svgButton.style.display = "block";
+    }
+}, 1000);
